109be1d44483b166ca4a66010e72c7e72570cedc
Jalix-WebTech <jalixwebtech@gmail.com>
2026-05-26 13:08:45 +0100

    final production deployment fix

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,9 @@
 import type { NextConfig } from "next";
 
-// 👇 use require (this fixes the error)
-const nextPWA = require("next-pwa");
-
-const withPWA = nextPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
+  turbopack: {},
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
